551be98c2a501a5f53140d891c64951ffa2a05cf
AliSohani <a.sohani.shahrood@gmail.com>
2023-04-16 21:09:01 +0330

    added profile_choise page after login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
+import { NextPageContext } from "next";
+import { signOut, getSession } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-      <h1 className="text-2xl text-green-500">netflix clone</h1>
+      <h1 className="text-4xl text-green-500">netflix clone</h1>
+      <p className="text-white">Loged in as {user?.name}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout
+      </button>
     </>
   );
 }
