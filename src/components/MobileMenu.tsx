2d2fe1061c9e0b06ce508bc521aa4480c4caa6a0
AliSohani <a.sohani.shahrood@gmail.com>
2023-05-03 12:25:52 +0330

    added a header

---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import headerMenu from "@/constants/headerMenu";
+
+interface MobileMenuProps {
+  visible?: boolean;
+}
+
+const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex">
+      <div className="flex flex-col gap-4">
+        {headerMenu.map(({ label }) => (
+          <div className="px-3 text-center text-white hover:underline">
+            {label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
---
+++ src/constants/headerMenu.ts
@@ -0,0 +1,33 @@
+type headerMenuProps = {
+  label: string;
+  url: string;
+};
+
+const headerMenu: headerMenuProps[] = [
+  {
+    label: "Home",
+    url: "/",
+  },
+  {
+    label: "Series",
+    url: "/",
+  },
+  {
+    label: "Films",
+    url: "/",
+  },
+  {
+    label: "New & Popular",
+    url: "/",
+  },
+  {
+    label: "My List",
+    url: "/",
+  },
+  {
+    label: "Browse by languages",
+    url: "/",
+  },
+];
+
+export default headerMenu;
